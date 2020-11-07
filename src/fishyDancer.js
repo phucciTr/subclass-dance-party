34d89f751fb0c1bf2e77913f035f4dcd8fb1cf33
James Henderson <jhende1995@gmail.com>
2020-11-07 16:14:37 -0500

    Complete two new dancers

---
+++ src/fishyDancer.js
@@ -0,0 +1,30 @@
+/*//Fishy Dancer Make function
+  //Make Fishy Dancer
+  //Add Fishy Dancer to list of All fishy Dancers
+  //Fishy Dancer start Dancing
+  //Fishy Dancer Set Postion
+  //return aNewDancer
+
+//FishyDancer constructor
+  //start with...
+    //random Color
+      //Red, Green, Blue? or any set of colors... up to you
+    //state of this fishy: Leader? Follower? Lost?
+
+//FishyDancer inheritance
+
+//FishyDancer Prototypes
+  //Methods
+    //Find the leader fish!
+
+    //Go to Leader Fish
+
+    //Follow leader fish
+
+    //Lead (Maybe copy wanderDancer step function here...)
+
+  //Values
+    //Object containing...
+      //a set of keys describing color of fish, and an array of matching fish
+      //a function to add a fish to the correct array
+*/
--- src/wanderDancer.js
+++ src/wanderDancer.js
@@ -1,5 +1,5 @@
 var makeWanderDancer = function(top, left) {
-  var aNewDancer = new WanderDancer(top, left, 33);
+  var aNewDancer = new WanderDancer(top, left, 1000 / 60);
 
   aNewDancer.setTargetAngle();
   aNewDancer.startDancing();
@@ -11,9 +11,10 @@ var makeWanderDancer = function(top, left) {
 var WanderDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
 
+  this.targetAngle = 0;
   this.currentAngle = Math.random() * 360;
-  this.turnSpeed = Math.random() * 20;
-  this.speed = Math.random() * 5 + 10;
+  this.turnSpeed = (Math.random() * 2) + 3;
+  this.speed = Math.random() * 5 + 3;
   this.topRange = $('body').height();
   this.leftRange = $('body').width();
 };
@@ -25,60 +26,55 @@ WanderDancer.prototype.constructor = WanderDancer;
 
 //methods
 WanderDancer.prototype.step = function() {
-  var nearTop = this.top < this.topRange * .2;
-  var nearBottom = this.top > this.topRange * .8;
-  var nearLeft = this.left < this.leftRange * .2;
-  var nearRight = this.left > this.leftRange * .8;
+  var nearTop = this.top < (this.topRange * .2);
+  var nearBottom = this.top > (this.topRange * .8);
+  var nearLeft = this.left < (this.leftRange * .1);
+  var nearRight = this.left > (this.leftRange * .9);
   var minNewAngle = undefined;
   var maxNewAngle = undefined;
 
   if (nearTop) {
-    minNewAngle = 0;
-    maxNewAngle = 180;
+    minNewAngle = 89;
+    maxNewAngle = 91;
   }
   if (nearBottom) {
-    minNewAngle = 180;
-    maxNewAngle = 360;
+    minNewAngle = 269;
+    maxNewAngle = 271;
   }
   if (nearLeft) {
-    if (nearTop) {
-      minNewAngle = 0;
-      maxNewAngle = 90;
-
-    } else if (nearBottom) {
-      minNewAngle = -90;
-      maxNewAngle = 0;
-
-    } else {
-      minNewAngle = -90;
-      maxNewAngle = 90;
-    }
+    minNewAngle = -1;
+    maxNewAngle = 1;
   }
   if (nearRight) {
-    if (nearTop) {
-      minNewAngle = 90;
-      maxNewAngle = 180;
-
-    } else if (nearBottom) {
-      minNewAngle = 180;
-      maxNewAngle = 270;
-
-    } else {
-      minNewAngle = 90;
-      maxNewAngle = 270;
-    }
+    minNewAngle = 179;
+    maxNewAngle = 181;
   }
   if (nearTop || nearBottom || nearLeft || nearRight) {
     this.setTargetAngle(minNewAngle, maxNewAngle);
     this.turn();
+    this.turn();
   }
-  // if (this.left > )
-  // if (this.left < )
 
   this.top = this.top + this.speed * Math.sin(this.currentAngle * Math.PI / 180);
   this.left = this.left + this.speed * Math.cos(this.currentAngle * Math.PI / 180);
   this.setPosition(this.top, this.left);
+  this.teleport();
   this.turn();
+};
+
+WanderDancer.prototype.teleport = function() {
+  if (this.top < -20) {
+    this.top = $('body').height() + 10;
+  }
+  if (this.top > $('body').height() + 20) {
+    this.top = -10;
+  }
+  if (this.left < -20) {
+    this.left = $('body').width() + 10;
+  }
+  if (this.left > $('body').width() + 20) {
+    this.left = - 10;
+  }
 };
 
 WanderDancer.prototype.setTargetAngle = function(min, max) {
@@ -97,7 +93,8 @@ WanderDancer.prototype.setTargetAngle = function(min, max) {
 };
 
 WanderDancer.prototype.turn = function() {
-  if (Math.abs(this.currentAngle - this.targetAngle) < this.turnSpeed * 2) {
+  if (Math.abs(this.currentAngle - this.targetAngle) < this.turnSpeed * 1.1) {
+    //this.setTargetAngle(this.currentAngle - 30, this.currentAngle + 30);
     this.setTargetAngle();
   }
 
